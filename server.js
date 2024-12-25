d833ec248f0f281450ea8206fc1457ad16166c0b
MV <mvasilev801@gmail.com>
2024-12-25 18:04:27 +0300

    Some fixes

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
     console.log(`Server is running on http://0.0.0.0:${port}`);
 });
